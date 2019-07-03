bf4d52752598f1d49042a0c48798b2a32894ee08
jonmoon74 <jon.moon@outlook.com>
2019-07-03 20:35:59 +0100

    Individual analysis script initial setup, test in jupyter

--- Individual assessment/analysis.py
+++ Individual assessment/analysis.py
@@ -1,58 +1,73 @@
 #  Imports and setup
 import pandas as pd
-import math
-
-# read in the excluded words list and create as a list
-exclusionFile = str(input("Enter the name of the file of words to exclude: "))
-excludedWordsDF = pd.read_csv(exclusionFile)
-ignored_words = excludedWordsDF['WORDS'].tolist()
-
-# read in the potential words list and create as a list
-potentialFile = str(input("Enter the name of the file of words used it indictae potential: "))
-potentialWordsDF = pd.read_csv(potentialFile)(potentialFile)
-potential_words = potentialWordsDF['WORDS'].tolist()
-
-# import the file to analyse
-mainDF = pd.read_excel("Talent data for analysis.xlsx", sheet_name="Sheet1")
-
-# words indicative of potential
-# Excel or csv file import
-# data to analyse
 
 # Import from excel to pandas data frame
+excludedWordsDF = pd.read_csv("excludedWords.csv")
+excWords = excludedWordsDF['WORDS'].tolist()
+potentialWordsDF = pd.read_csv("testWords.csv")
+potWords = potentialWordsDF['WORDS'].tolist()
+mainData = pd.read_excel("Talent data for analysis.xlsx")
+
 
 # run the analysis
-
-# Loop by line
-# Assign strengths content to variable
-# Assign development areas content to variable
-
-# Loop through strengths words,
-# Count all words
-# Exclude ignore words and count ignored
-# Count words in indicative potential list
-# Calculate percentage of potential words and return to new data frame column
-# Reset counters
-
-# Repeat for development areas words
-
-# Calculate difference between strengths and dev areas and return to new column
-
-# If difference positive to strengths indicate potential talent in new column, else nothing
-
-# If potential talent indicate readiness based on difference percentage points (agree with Ruth)
+def strengthsResults(textData):
+    potWordsCount = 0
+    excWordsCount = 0
+    checkWordsCount = 0
+    for word in textData.split():
+        word = word.lower()
+        if word in excWords:
+            excWordsCount += 1
+        elif word in potWords:
+            potWordsCount += 1
+        else:
+            checkWordsCount += 1
+    fullCount = potWordsCount + checkWordsCount
+    strengthsOut = (excWordsCount, potWordsCount, fullCount)
+    return strengthsOut
 
 
-def strengths_count():
-    pass
+def devAreaResults(textData2):
+    potWordsCount = 0
+    excWordsCount = 0
+    checkWordsCount = 0
+    for word in textData2.split():
+        word = word.lower()
+        if word in excWords:
+            excWordsCount += 1
+        elif word in potWords:
+            potWordsCount += 1
+        else:
+            checkWordsCount += 1
+    fullCount = potWordsCount + checkWordsCount
+    devAreasOut = (excWordsCount, potWordsCount, fullCount)
+    return devAreasOut
 
 
-def development_areas_count():
-    pass
+def calcFields(strengthsOut, devAreasOut):
+    marker = ""
+    readyMarker = ""
+    strengthDensity = round(((strengthsOut[1] / strengthsOut[2]) * 100), 2)
+    devAreaDensity = round(((devAreasOut[1] / devAreasOut[2]) * 100), 2)
+    difference = strengthDensity - devAreaDensity
+    if difference > 0:
+        marker = "Potential Talent"
+        if difference > 1.75:
+            readyMarker = "Ready"
+        else:
+            readyMarker = "To develop"
+
+    calcOut = (strengthDensity, devAreaDensity, difference, marker, readyMarker)
+    mainData['Strengths Density'] = calcOut[0]
+    mainData['Developement Areas Density'] = calcOut[1]
+    mainData['Difference'] = calcOut[2]
+    mainData['Potential Talent?'] = calcOut[3]
+    mainData['Readiness'] = calcOut[4]
+    return calcOut
 
 
-def density_calc():
-    pass
-
+for index in mainData.head():
+    calcFields(strengthsResults(str(mainData['Strengths'])), devAreaResults(str(mainData['Development Areas'])))
 
 # output the result to Excel
+mainData.to_excel("Individual Analysis Outputs.xlsx")
